9beda041a4d4de610a9352cd33adb1b267f4627b
sanjanadubariya <psanjana187@gmail.com>
2025-08-05 18:38:30 +0530

    Updated features or fixed bugs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <Router>    
-        <div style={{ position: "relative"}}>
+        <div>
           <div className="App">
             <Navbar/>
             <Routes>
